6dd520a56d7264c75475e8e603d25fa2d7fea824
Sonya Slegers <sonjay08@gmail.com>
2018-07-26 14:43:27 -0500

    singly linked list algos

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const util = require('util');
+
 class _Node {
     constructor(value, next) {
         this.value = value;
@@ -73,11 +75,107 @@ class LinkedList {
 
         return currentNode;
     }
+
+    insertBefore(node, value) {
+        if (!this.head) {
+            return null;
+        }
+        //this.head = 0, next == 1
+        let previousNode = this.head // == 0
+        let currentNode = this.head; // == 0
+        //console.log(currentNode, node); 
+        //currentNode.next should probably not equal null. if it is null it will keep trying to find it by re-assigning the variables.
+        //so if currentNode.val is 5 and node = 3. that is true because !== and if currentNode.next = null , the second part is true as well which will make it do the actions after. assigning the variables. so this will loop forever (i think)
+
+        //if this.head === value, create, next = this.head, this.head = new node
+        //start = 0
+        //next = 0,
+        //start 0, next 1
+        
+        if (previousNode.value === node) {
+        	return this.insertFirst(value);
+        }
+
+        while (currentNode.value !== node && currentNode.next !== null) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        if (currentNode.next === null) {
+            console.log(`${node} not found`);
+            return;
+        } else {
+            previousNode.next = new _Node(value, currentNode);
+        }
+    }
+
+    insertAfter(node, value) {
+        // find the node
+        // use while loop to search LL for node
+        // if the node is not found, console log a message
+        // if it's found, set the previous node to the value and point it at current.next
+        let previousNode = this.head;
+        let currentNode = this.head;
+
+        if (previousNode === node) {
+            return this.insertFirst(value);
+        }
+
+        while (currentNode.value !== node && currentNode.next !== null) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        if (currentNode.next === null) {
+            console.log(`${node} not found`);
+            return;
+        } else {
+            //console.log('previousNode.next:', previousNode.next)
+            previousNode.next = new _Node(value, previousNode.next)
+        }
+        
+    }
+
+    insertAt(newValue, location) {
+        // find the location
+        // use while loop to find the location in the LL
+        // when location is found, set the previous.next to the newValue with newValue pointing to current.next
+
+        let previousNode = this.head;
+        let currentNode = this.head;
+        let counter = 1;
+
+        if (previousNode.value === newValue) {
+            return this.insertFirst(newValue)
+        }
+
+        while (counter !== location  && currentNode.next !== null) {
+            previousNode = currentNode; 
+            currentNode = currentNode.next; 
+            counter++;
+        }
+        if (currentNode.next === null) {
+            console.log(`${location} not found`);
+        return;
+        } else {
+            previousNode.next = new _Node(newValue, currentNode.next)
+        }
+    }
 }
 
 function main() {
-    
+    let SLL = new LinkedList();
+    SLL.insertFirst('Apollo');
+    SLL.insertFirst('Boomer');
+    SLL.insertFirst('Helo');
+    SLL.insertFirst('Husker');
+    SLL.insertFirst('Starbuck');
+    SLL.insertFirst('Tauhida');
+    SLL.remove('squirrel');
+    SLL.insertBefore('Boomer', 'Athena');
+    SLL.insertAfter('Helo', 'HotDog');
+    SLL.insertAt('Kat', 3);
+    SLL.remove('Tauhida');
 
+    console.log(util.inspect(SLL, false, null));
 }
 
 main();
